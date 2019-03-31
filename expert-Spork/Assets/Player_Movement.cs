a4eaebb21cd7b4e602f9dcb850325b8625bb604e
2GoodPhoU <ericsy99@gmail.com>
2019-03-31 16:16:50 -0700

    Level01.1
    Created a new Scene. Starting physics

---
+++ expert-Spork/Assets/Player_Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    // We marked this as "Fixed" Update because we
+    // are using it to mess with physics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
