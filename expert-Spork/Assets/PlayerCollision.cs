17a3bb153a78cb381b4e18a6fa29537225ea2f58
2GoodPhoU <ericsy99@gmail.com>
2019-03-31 17:03:36 -0700

    Level01.2
    following camera, added obstacle.

---
+++ expert-Spork/Assets/PlayerCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Player_Movement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- expert-Spork/Assets/Player_Movement.cs
+++ expert-Spork/Assets/Player_Movement.cs
@@ -5,6 +5,9 @@ public class Player_Movement : MonoBehaviour
     // This is a reference to the Rigidbody component called "rb"
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
+    public float sidwaysForce = 500f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +22,19 @@ public class Player_Movement : MonoBehaviour
 
     // We marked this as "Fixed" Update because we
     // are using it to mess with physics
+    // should use variables and check for pressed key in the update instead
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidwaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidwaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
