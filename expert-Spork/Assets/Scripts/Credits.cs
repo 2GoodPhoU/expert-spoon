745b5ce436841f7b1da204cb3ae1d07196127092
2GoodPhoU <ericsy99@gmail.com>
2019-03-31 22:38:41 -0700

    Complete Game
    WOOT WOOT
    -Scenes
    -Menu
    -Credits
    -3 Levels
    FUN!

---
+++ expert-Spork/Assets/Scripts/Credits.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ expert-Spork/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
